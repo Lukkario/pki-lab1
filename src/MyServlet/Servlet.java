17a48d3906cc5eb41ec143658f95b3c282378961
Lukkario <michallukianski.0@gmail.com>
2019-03-13 20:51:34 +0100

    init

---
+++ src/MyServlet/Servlet.java
@@ -0,0 +1,28 @@
+package MyServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+        try
+        {
+            pw.println("<h2>Welcome to my page.</h2>");
+        }
+        finally {
+            pw.close();
+        }
+    }
+}
